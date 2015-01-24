1a5203d04862e01ccbf1a096a1a7fefcce08e52d
HeavenlyAwe <christoffer.fridlund@hotmail.com>
2015-01-24 17:00:31 +0200

    Implemented event class.

--- game/event.lua
+++ game/event.lua
@@ -2,12 +2,28 @@ EventPrms = {
 }
 
 Event = {
+  type = "default",
+  panicValue = 5,
+  awkwardValue = 5,
 }
 Event.__index = Event
 
 
 function Event:new(panicValue, awkwardValue, o)
   local self = setmetatable(o or {}, Event)
-
+  
+  self.panicValue = panicValue or self.panicValue
+  self.awkwardValue = awkwardValue or self.awkwardValue
+  
   return self
+end
+
+
+function Event:getPanicValue()
+  return self.panicValue
+end
+
+
+function Event:getAwkwardValue()
+  return self.awkwardValue
 end
