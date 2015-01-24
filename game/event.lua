6f36b5ee1f04ba4af86b140e1549a24cb047646c
HeavenlyAwe <christoffer.fridlund@hotmail.com>
2015-01-24 17:11:08 +0200

    Rearranged the event constructor

--- game/event.lua
+++ game/event.lua
@@ -1,20 +1,26 @@
 EventPrms = {
-}
+  }
 
 Event = {
-  type = "default",
+  type = nil,
   panicValue = 5,
   awkwardValue = 5,
 }
 Event.__index = Event
 
 
-function Event:new(panicValue, awkwardValue, o)
+function Event:new(type, panicValue, awkwardValue, o)
   local self = setmetatable(o or {}, Event)
-  
-  self.panicValue = panicValue or self.panicValue
+
+  self.type = type
+  if panicValue ~= nil and awkwardValue == nil then
+    self.panicValue = panicValue
+    self.awkwardValue = panicValue
+  end
+
+  self.panicValue = panicValue
   self.awkwardValue = awkwardValue or self.awkwardValue
-  
+
   return self
 end
 
