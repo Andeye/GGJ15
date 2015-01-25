3595c2d22de12a2a5a4fb22162771b2a6b733598
Lasse Lybeck <lybeck.lasse@gmail.com>
2015-01-25 16:15:58 +0200

    Updated the main character spritesheets.

--- game/sound_sfx.lua
+++ game/sound_sfx.lua
@@ -6,7 +6,6 @@ SoundSfx = {
 function SoundSfx:load()
   local dir = "assets/sounds/sfx"
   local files = love.filesystem.getDirectoryItems(dir)
-  print("Loading sound effects:")
   for _, f in ipairs(files) do
   
     -- assuming that the extension is 3 characters long
@@ -16,18 +15,16 @@ function SoundSfx:load()
     
     local filename = string.sub(f, 0, #f - 4)
     local extension = string.sub(f, #f - 2, #f)
-    print("  " .. filename)
     self.sounds[filename] = love.audio.newSource(dir .. "/" .. filename .. "." .. extension, "static")
   end
   
   -- set specific sound volumes
-  self:setVolume("button_click", .15)
-  self:setVolume("button_release", .6)
+  self:setVolume("button_click", .08)
+  self:setVolume("button_release", .3)
 end
 
 
 function SoundSfx:play(key)
-  print("Playing sound: " .. key)
   if self.sounds[key] then
     self.sounds[key]:play()
   end
